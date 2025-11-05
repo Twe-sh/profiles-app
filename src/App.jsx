c8ca7bbc54a975477a95bdd4fcbcb57b354e48c5
Twe-sh <112032683+Twe-sh@users.noreply.github.com>
2025-11-05 20:35:39 +0000

    added button to add likes and fixed size of container

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,23 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ProfileCard from './components/ProfileCard.jsx';
 import { profiles } from './data/profiles.js';
+import { useState } from 'react'
 
 export default function App() {
+  const [people, setPeople] = useState(profiles);
+
+  function onLikeClicked(id) {
+    console.log(id)
+    setPeople(ps => ps.map(p => p.id===id? { ...p, likes: p.likes+1 } : p))
+  }
+
   return (
-    <Container className="py-4">
+    <Container className="py-4 container-fluid">
       <h1 className="mb-4 text-center">Profiles</h1>
       <Row xs={1} md={2} lg={3}>
-        {profiles.map(p => (
+        {people.map(p => (
           <Col key={p.id}>
-            <ProfileCard name={p.name} likes={p.likes} />
+            <ProfileCard name={p.name} likes={p.likes} id={p.id} onLikeClicked={onLikeClicked}/>
           </Col>
         ))}
       </Row>
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,12 +1,17 @@
 import Card from 'react-bootstrap/Card';
 
-export default function ProfileCard({ name, likes }) {
-  return (
-    <Card className="mb-3 shadow-sm">
-      <Card.Body>
-        <Card.Title className="h5 mb-1">{name}</Card.Title>
-        <Card.Text className="mb-0">Likes: {likes}</Card.Text>
-      </Card.Body>
-    </Card>
-  );
+export default function ProfileCard({ name, likes, id, onLikeClicked }) {
+    function likeClicked() {
+        onLikeClicked(id)
+    }
+
+    return (
+        <Card className="mb-3 shadow-sm ">
+        <Card.Body>
+            <Card.Title className="h5 mb-1">{name}</Card.Title>
+            <Card.Text className="mb-0">Likes: {likes}</Card.Text>
+            <button onClick={likeClicked}>Add Like</button>
+        </Card.Body>
+        </Card>
+    );
 }
